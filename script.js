e898c3599f929183f7ddd2cdfd7ff7106429f8aa
Martin Ueding <dev@martin-ueding.de>
2013-09-12 17:27:54 +0200

    Add Berlin Address search

--- script.js
+++ script.js
@@ -14,6 +14,29 @@ function initialize() {
 		title: "Test",
 		icon: "http://martin-ueding.de/_static/arrow.png"
 	});
+
+	toBerlin();
 }
+
+toBerlin = function() {
+	var address = "Bundestag, Berlin";
+
+	var geocoder = new google.maps.Geocoder();
+	var searchParams = {address: address};
+	var callback = function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+				map: map,
+				position: results[0].geometry.location
+			});
+		}
+		else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+
+	};
+	geocoder.geocode(searchParams, callback);
+};
 
 google.maps.event.addDomListener(window, 'load', initialize);
